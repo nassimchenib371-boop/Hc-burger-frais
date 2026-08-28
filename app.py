632e3af344b29e06df711d2b40b7d9e6e4ddfe92
nassimchenib371-boop <nassimchenib371@icloud.com>
2026-08-28 19:45:02 +0200

    fix da tabase initialization

--- app.py
+++ app.py
@@ -314,7 +314,7 @@ def ticket(oid):
     con=db(); o=con.execute("SELECT * FROM orders WHERE id=?",(oid,)).fetchone(); con.close()
     if not o: return "Introuvable",404
     return render_template("ticket.html",o=o,items=json.loads(o["items_json"]))
-
+init_db()
 if __name__=="__main__":
-    init_db()
+    
     app.run(host="0.0.0.0",port=int(os.getenv("PORT","8000")),debug=False)
