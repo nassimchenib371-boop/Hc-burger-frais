c893f9c1496b0bf4a7575b916335f6c72e948401
nassimchenib371-boop <nassimchenib371@icloud.com>
2026-08-31 15:57:39 +0200

    add menu option + 2.50€

--- app.py
+++ app.py
@@ -418,9 +418,8 @@ con.execute(
     con.commit()
     con.close()
     return redirect(url_for("home"))
-
-    con.close()
-    return render_template("order.html", product=product)
+con.close()
+return render_template("order.html", product=product)
 init_db()
 if __name__=="__main__":
     
