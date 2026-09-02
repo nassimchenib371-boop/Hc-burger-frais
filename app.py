d32fcee54993025a42bef21889243377e95332b2
nassimchenib371-boop <nassimchenib371@icloud.com>
2026-09-02 13:04:11 +0200

    fix

--- app.py
+++ app.py
@@ -314,7 +314,7 @@ def ticket(oid):
     con=db(); o=con.execute("SELECT * FROM orders WHERE id=?",(oid,)).fetchone(); con.close()
     if not o: return "Introuvable",404
     return render_template("ticket.html",o=o,items=json.loads(o["items_json"]))
-    @app.context_processor
+ @app.context_processor
 def inject_cart_count():
     cart_data = session.get("cart", {})
     cart_count = sum(int(q) for q in cart_data.values())
